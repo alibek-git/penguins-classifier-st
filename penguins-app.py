499056c7a8cb4c6b0ee0efa67a8f8c63284e1492
Alibek Zhubekov <alibek1991@gmail.com>
2023-05-27 13:40:00 +0600

    Modify main app code to load styles

--- penguins-app.py
+++ penguins-app.py
@@ -6,105 +6,105 @@ from PIL import Image
 from sklearn.ensemble import RandomForestClassifier
 
 # st.polyfill('BigInt64Array')
+def main():
 
-st.set_page_config(
-    page_title="Penguin Classifier App",
-    page_icon="lter_penguins.png",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
+    st.set_page_config(
+        page_title="Penguin Classifier App",
+        page_icon="lter_penguins.png",
+        layout="wide",
+#        initial_sidebar_state="expanded"
+    )
 
-image = Image.open('lter_penguins.png')
+    image = Image.open('lter_penguins.png')
 
-st.image(image, use_column_width=True)
+    st.image(image, use_column_width=True)
 
-st.write("""
-# Penguin Prediction App
+    st.write("""
+    # Penguin Prediction App
 
-This app predicts the **Palmer Penguin** species!
+    This app predicts the **Palmer Penguin** species!
 
-Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
-""")
+    Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
+    """)
 
-st.sidebar.header('User Input Features')
+    st.sidebar.header('User Input Features')
 
-st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
-""")
+    st.sidebar.markdown("""
+    [Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+    """)
 
-# Collects user input features into dataframe
-uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-if uploaded_file is not None:
-    input_df = pd.read_csv(uploaded_file)
-else:
-    def user_input_features():
-        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-        sex = st.sidebar.selectbox('Sex',('male','female'))
-        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-        data = {'island': island,
-                'bill_length_mm': bill_length_mm,
-                'bill_depth_mm': bill_depth_mm,
-                'flipper_length_mm': flipper_length_mm,
-                'body_mass_g': body_mass_g,
-                'sex': sex}
-        features = pd.DataFrame(data, index=[0])
-        return features
-    input_df = user_input_features()
+    # Collects user input features into dataframe
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+    if uploaded_file is not None:
+        input_df = pd.read_csv(uploaded_file)
+    else:
+        def user_input_features():
+            island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
+            sex = st.sidebar.selectbox('Sex',('male','female'))
+            bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
+            bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
+            flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
+            body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
+            data = {'island': island,
+                    'bill_length_mm': bill_length_mm,
+                    'bill_depth_mm': bill_depth_mm,
+                    'flipper_length_mm': flipper_length_mm,
+                    'body_mass_g': body_mass_g,
+                    'sex': sex}
+            features = pd.DataFrame(data, index=[0])
+            return features
+        input_df = user_input_features()
 
-# Combines user input features with entire penguins dataset
-# This will be useful for the encoding phase
-penguins_raw = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-penguins = penguins_raw.drop(columns=['species'], axis=1)
-df = pd.concat([input_df,penguins],axis=0)
+    # Combines user input features with entire penguins dataset
+    # This will be useful for the encoding phase
+    penguins_raw = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+    penguins = penguins_raw.drop(columns=['species'], axis=1)
+    df = pd.concat([input_df,penguins],axis=0)
 
-# Encoding of ordinal features
-# https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
-encode = ['sex','island']
-for col in encode:
-    dummy = pd.get_dummies(df[col], prefix=col)
-    df = pd.concat([df,dummy], axis=1)
-    del df[col]
-df = df[:1] # Selects only the first row (the user input data)
+    # Encoding of ordinal features
+    # https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
+    encode = ['sex','island']
+    for col in encode:
+        dummy = pd.get_dummies(df[col], prefix=col)
+        df = pd.concat([df,dummy], axis=1)
+        del df[col]
+    df = df[:1] # Selects only the first row (the user input data)
 
-# Displays the user input features
-st.subheader('User Input features')
+    # Displays the user input features
+    st.subheader('User Input features')
 
-if uploaded_file is not None:
-    st.write(df)
-else:
-    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-    st.write(df)
+    if uploaded_file is not None:
+        st.write(df)
+    else:
+        st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
+        st.write(df)
 
-# Reads in saved classification model
-load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
+    # Reads in saved classification model
+    load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
 
-# Apply model to make predictions
-prediction = load_clf.predict(df)
-prediction_proba = load_clf.predict_proba(df)
+    # Apply model to make predictions
+    prediction = load_clf.predict(df)
+    prediction_proba = load_clf.predict_proba(df)
 
 
-st.subheader('Prediction')
-penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
-st.write(penguins_species[prediction])
+    st.subheader('Prediction')
+    penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
+    st.write(penguins_species[prediction])
 
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+    st.subheader('Prediction Probability')
+    st.write(prediction_proba)
 
 if __name__ == '__main__':
     main()
-    
-    # Add CSS stylesheet
-    st.markdown(
-        """
-        <style>
-        .reportview-container {
-            background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 100 100'%3E%3Crect width='100' height='100' fill='none'%3E%3C/rect%3E%3Ccircle cx='50' cy='50' r='40' fill='%23your-color'/%3E%3C/svg%3E") no-repeat center center fixed;
-            background-size: cover;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+
+# Add CSS stylesheet
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #6699CC;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
